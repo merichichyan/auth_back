c6246483f1a5dac9b8561d82bef553b3af32748e
Meri Chichyan <merichichyan@Ashots-MacBook-Pro.local>
2026-05-12 12:21:39 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using SecureNotesApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// DB
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "API is running");
+
+app.Run();
